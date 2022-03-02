b753f8ae673e5d930773e4366a533309fe6c4b00
ULI-008 <A01657285@itesm.mx>
2022-03-01 23:27:27 -0600

    Accion Right en el cubo
    Las 6 caras se editan cuando giras 90 grados el lado derecho del cubo al sentido del reloj

--- VirtualRubiksCube.py
+++ VirtualRubiksCube.py
@@ -20,37 +20,61 @@ x = str(input("\n\nMovimiento: "))
 if x == "r":
     
     wx = array(w)
-    rx = array(o)
+    rx = array(r)
     yx = array(y)
-    ox = array(r)
+    ox = array(o)
+    bx = array(b)
+    gx = array(g)
     
-    w[0,0]= rx[0,0]
-    w[1,0]= rx[1,0]
-    w[2,0]= rx[2,0]
+    w[0,2]= ox[0,2]
+    w[1,2]= ox[1,2]
+    w[2,2]= ox[2,2]
     
-    r[0,0]= yx[0,0]
-    r[1,0]= yx[1,0]
-    r[2,0]= yx[2,0]
+    r[0,2]= wx[0,2]
+    r[1,2]= wx[1,2]
+    r[2,2]= wx[2,2]
     
-    y[0,0]= ox[0,0]
-    y[1,0]= ox[1,0]
-    y[2,0]= ox[2,0]
+    y[0,2]= rx[0,2]
+    y[1,2]= rx[1,2]
+    y[2,2]= rx[2,2]
     
-    o[0,0]= wx[0,0]
-    o[1,0]= wx[1,0]
-    o[2,0]= wx[2,0]
-    
-else:
-    print("no")
-    
-    
+    o[0,2]= yx[0,2]
+    o[1,2]= yx[1,2]
+    o[2,2]= yx[2,2]
+
+    b[0,0]= bx[0,0]
+    b[0,1]= bx[0,1]
+    b[0,2]= bx[0,2]
+    b[1,0]= bx[1,0]
+    b[1,2]= bx[1,2]
+    b[2,0]= bx[2,0]
+    b[2,1]= bx[2,1]
+    b[2,2]= bx[2,2]
+
+    g[0,0]= gx[2,0]
+    g[0,1]= gx[1,0]
+    g[0,2]= gx[0,0]
+    g[1,0]= gx[2,1]
+    g[1,2]= gx[0,1]
+    g[2,0]= gx[2,2]
+    g[2,1]= gx[1,2]
+    g[2,2]= gx[0,2]
+
     print("\n\n ")
     print(wx)
     print(rx)
     print(yx)
+    print(ox)
     print(bx)
+    print(gx)
     print("\n\n ")
     print(w)
     print(r)
     print(y)
+    print(o)
     print(b)
+    print(g)
+    
+else:
+    print("no")
+    
